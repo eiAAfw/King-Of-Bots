9520da81cb90752ceed43193d877c001c1cd8de0
wfi <wfi1202@163.com>
2022-11-30 23:23:26 +0800

    项目骨架

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,18 +4,18 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedList;
+import java.util.*;
 import java.util.List;
-
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
-    @RequestMapping("getBotInfo/")
-    public List<String> getbotinfo(){
-        List<String> list = new LinkedList<>();
-        list.add("apple");
-        list.add("bannana");
+    @RequestMapping("getbotinfo/")
+    public Map<String,String> getbotinfo(){
+    Map<String,String> bot1 = new HashMap<>();
+    bot1.put("name","tiger");
+    bot1.put("rating","1500");
+    return bot1;
 
-        return list;
+
     }
 }
